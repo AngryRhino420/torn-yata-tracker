e609f06bade1a11529d863fcca0c70428788f5ff
AngryRhino420 <krstevskislika@gmail.com>
2026-06-28 14:15:45 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+TRACKED = {
+
+    "uni": {
+
+        "country":"UK",
+        "item":"Xanax",
+        "id":206,
+        "travel":20
+    },
+
+
+    "jap": {
+
+        "country":"Japan",
+        "item":"Xanax",
+        "id":206,
+        "travel":30
+    }
+
+}
+
+
+CET = "Europe/Berlin"
